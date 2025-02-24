1ee51480e60f5382be60e62c840cb187bccb4e48
3joker1789 <rv.saritamac@uea.edu.ec>
2025-02-23 20:17:16 -0500

    mi proyecto flask

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def home():
-    return '¡Hola,Amigos estamos de vuelta!'
+    return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
-@app.route('/about')
-def about():
-    return 'Esta es la página Acerca de'
