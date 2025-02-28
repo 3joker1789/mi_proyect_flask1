47365c2c3ca0fba9101b4fd755c3b834f8cc90d5
3joker1789 <rv.saritamac@uea.edu.ec>
2025-02-27 23:21:37 -0500

    proyecto flask

--- app.py
+++ app.py
@@ -12,3 +12,23 @@ def about():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    from flask_wtf import FlaskForm
+    from wtforms import StringField, SubmitField
+    from wtforms.validators import DataRequired
+
+
+    class NameForm(FlaskForm):
+        name = StringField('¿Cuál es tu nombre?', validators=[DataRequired()])
+        submit = SubmitField('Enviar')
+        from flask import Flask, render_template
+
+        app = Flask(__name__)
+        app.config['SECRET_KEY'] = 'supersecretkey'  # Required for CSRF protection
+
+        @app.route('/form', methods=['GET', 'POST'])
+        def form():
+            form = NameForm()
+            if form.validate_on_submit():
+                name = form.name.data
+                return render_template('resultado.html', name=name)
+            return render_template('formulario.html', form=form)
