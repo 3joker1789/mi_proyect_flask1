2898e31ba21dcbc7443d842194eeb2242a14e6ac
3joker1789 <rv.saritamac@uea.edu.ec>
2025-04-06 21:12:45 -0500

    proyecto flask 2.0

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask_login import login_required
 from flask_mysqldb import MySQL
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
